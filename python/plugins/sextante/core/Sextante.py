carsmie/QGIS

commit dbfdd544886505f2c7335b707615a162c8e70681

Several patches contributed by Rado Guzinski: -New OTB descriptions an help -Update OTB descriptions -More responsive MultipleInputPanel.py -Minor bug fixes

python/plugins/sextante/core/Sextante.py
@@ -17,6 +17,7 @@
 ***************************************************************************
 """
 
+
 __author__ = 'Victor Olaya'
 __date__ = 'August 2012'
 __copyright__ = '(C) 2012, Victor Olaya'
@@ -25,6 +26,7 @@ __revision__ = '$Format:%H$'
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from sextante.wps.WPSAlgorithmProvider import WPSAlgorithmProvider
 from sextante.saga.SagaAlgorithmProvider import SagaAlgorithmProvider
 from sextante.script.ScriptAlgorithmProvider import ScriptAlgorithmProvider
 from sextante.core.QGisLayers import QGisLayers
@@ -121,6 +123,7 @@ class Sextante:
         Sextante.addProvider(SagaAlgorithmProvider())
         Sextante.addProvider(GrassAlgorithmProvider())
         Sextante.addProvider(ScriptAlgorithmProvider())
+        Sextante.addProvider(WPSAlgorithmProvider())
         Sextante.modeler.initializeSettings();
         #and initialize
         SextanteLog.startLogging()

commit 9fe4be19457bf687deeb148c83b76627f35edb70

fix SEXTANTE crash on load caused by dbfdd54488

python/plugins/sextante/core/Sextante.py
@@ -26,7 +26,6 @@ __revision__ = '$Format:%H$'
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-from sextante.wps.WPSAlgorithmProvider import WPSAlgorithmProvider
 from sextante.saga.SagaAlgorithmProvider import SagaAlgorithmProvider
 from sextante.script.ScriptAlgorithmProvider import ScriptAlgorithmProvider
 from sextante.core.QGisLayers import QGisLayers
@@ -123,7 +122,6 @@ class Sextante:
         Sextante.addProvider(SagaAlgorithmProvider())
         Sextante.addProvider(GrassAlgorithmProvider())
         Sextante.addProvider(ScriptAlgorithmProvider())
-        Sextante.addProvider(WPSAlgorithmProvider())
         Sextante.modeler.initializeSettings();
         #and initialize
         SextanteLog.startLogging()