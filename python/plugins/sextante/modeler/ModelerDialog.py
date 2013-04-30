carsmie/QGIS

commit 1351f09d802e8b95b65c827c244a0a5f99ec0225

[sextante] Added checking in case model file cannot be save due to permissions

python/plugins/sextante/modeler/ModelerDialog.py
@@ -16,6 +16,7 @@
 *                                                                         *
 ***************************************************************************
 """
+import sys
 
 __author__ = 'Victor Olaya'
 __date__ = 'August 2012'
@@ -154,6 +155,7 @@ class ModelerDialog(QDialog, Ui_DlgModeler):
             dlg.exec_()
 
     def save(self):
+        if 
         self.saveModel(False)
 
     def saveAs(self):
@@ -179,8 +181,20 @@ class ModelerDialog(QDialog, Ui_DlgModeler):
                 self.alg.descriptionFile = filename
         if filename:
             text = self.alg.serialize()
-            fout = codecs.open(filename, "w", encoding='utf-8')
-            #fout = open(filename, "w")
+            try:
+                fout = codecs.open(filename, "w", encoding='utf-8')
+            except:
+                if saveAs:
+                    QMessageBox.warning(self,
+                                    self.tr("I/O error"),
+                                    self.tr("Unable to save edits. Reason:\n %1").arg(unicode(sys.exc_info()[1]))
+                                   )
+                else:
+                    QMessageBox.warning(self,
+                                    self.tr("Can't save model"),
+                                    self.tr("This model can't be saved in its original location\n(probably you do not have permission to do it).\nPlease, use the 'Save as...' option.")
+                                   )
+                return
             fout.write(text)
             fout.close()
             self.update = True

commit 46f6a8f4c00e85ff5195b903c8bbda29c1a7058f

[sextante] fixed blocking typo introduced in 1351f09d802e8b

python/plugins/sextante/modeler/ModelerDialog.py
@@ -155,7 +155,6 @@ class ModelerDialog(QDialog, Ui_DlgModeler):
             dlg.exec_()
 
     def save(self):
-        if 
         self.saveModel(False)
 
     def saveAs(self):