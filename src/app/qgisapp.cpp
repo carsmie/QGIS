carsmie/QGIS

commit 24f07b88d284e4bf15a4507005c2a6f150b5c045

QGisApp: fix potential crash at application closing linked to mDataSourceManagerDialog destruction closing GDAL datasets after GDAL has been shut down

src/app/qgisapp.cpp
@@ -1317,8 +1317,6 @@ QgisApp::~QgisApp()
   // cancel request for FileOpen events
   QgsApplication::setFileOpenEventReceiver( nullptr );
 
-  QgsApplication::exitQgis();
-
   delete QgsProject::instance();
 
   delete mPythonUtils;
@@ -1326,6 +1324,12 @@ QgisApp::~QgisApp()
   delete mTray;
 
   delete mDataSourceManagerDialog;
+
+  // This function *MUST* be the last one called, as it destroys in
+  // particular GDAL. As above objects can hold GDAL/OGR objects, it is not
+  // safe destroying them afterwards
+  QgsApplication::exitQgis();
+  // Do *NOT* add anything here !
 }
 
 void QgisApp::dragEnterEvent( QDragEnterEvent *event )

commit 1299f3a868087054c63ece25a35169877d97964d

Application termination: fix crash caused by 24f07b88d284e4bf15a4507005c2a6f150b5c045 when layers are still opened at exit

src/app/qgisapp.cpp
@@ -1317,8 +1317,6 @@ QgisApp::~QgisApp()
   // cancel request for FileOpen events
   QgsApplication::setFileOpenEventReceiver( nullptr );
 
-  delete QgsProject::instance();
-
   delete mPythonUtils;
 
   delete mTray;

src/core/qgsapplication.cpp
@@ -891,10 +891,12 @@ void QgsApplication::exitQgis()
   //LeakSanitiser noise which hides real issues
   QgsApplication::sendPostedEvents( nullptr, QEvent::DeferredDelete );
 
-  delete QgsProviderRegistry::instance();
-
   //delete all registered functions from expression engine (see above comment)
   QgsExpression::cleanRegisteredFunctions();
+
+  delete QgsProject::instance();
+
+  delete QgsProviderRegistry::instance();
 
   // tear-down GDAL/OGR
   OGRCleanupAll();

src/core/qgsproject.cpp
@@ -373,6 +373,10 @@ QgsProject::~QgsProject()
   delete mRelationManager;
   delete mLayerTreeRegistryBridge;
   delete mRootGroup;
+  if ( this == sProject )
+  {
+    sProject = nullptr;
+  }
 }
 
 

src/core/qgsproviderregistry.cpp
@@ -45,11 +45,19 @@ typedef QString protocolDrivers_t();
 //typedef int dataCapabilities_t();
 //typedef QgsDataItem * dataItem_t(QString);
 
-
+static QMutex mutex;
+static QgsProviderRegistry *sInstance = nullptr;
 
 QgsProviderRegistry *QgsProviderRegistry::instance( const QString &pluginPath )
 {
-  static QgsProviderRegistry *sInstance( new QgsProviderRegistry( pluginPath ) );
+  if ( !sInstance )
+  {
+    QMutexLocker locker( &mutex );
+    if ( !sInstance )
+    {
+      sInstance = new QgsProviderRegistry( pluginPath );
+    }
+  }
   return sInstance;
 } // QgsProviderRegistry::instance
 
@@ -256,6 +264,8 @@ void QgsProviderRegistry::clean()
 QgsProviderRegistry::~QgsProviderRegistry()
 {
   clean();
+  if ( sInstance == this )
+    sInstance = nullptr;
 }
 
 