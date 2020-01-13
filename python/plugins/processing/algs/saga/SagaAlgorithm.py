carsmie/QGIS

commit f3fd0c4c4d8a37004655e047524af4876ca6a950

[processing] fixed SAGA command writing and test

python/plugins/processing/algs/saga/SagaAlgorithm.py
@@ -321,8 +321,7 @@ class SagaAlgorithm(SagaAlgorithmBase):
 
             output_files[out.name()] = filePath
             command += ' -{} "{}"'.format(out.name(), filePath)
-
-        commands.append(command)
+            commands.append(command)
 
         # special treatment for RGB algorithm
         # TODO: improve this and put this code somewhere else

python/plugins/processing/tests/SagaAlgorithmsTest.py
@@ -122,7 +122,7 @@ class TestSagaAlgorithms(unittest.TestCase, AlgorithmsTestBase.AlgorithmsTest):
                       'DIST_FIELD_DEFAULT': 5,
                       'NZONES': 1,
                       'DARC': 5,
-                      'DISSOLVE': True,
+                      'DISSOLVE': False,
                       'POLY_INNER': False,
                       'BUFFER': temp_file}
         feedback = QgsProcessingFeedback()

commit a6fcf81c7eb12beff7b5243a9b93e89556407f50

[SAGA] Fix saga_cmd command with multiple outputs
Fixes a regression bug accidentally introduced with f3fd0c4c4d8a37004655e047524af4876ca6a950 (PR #8968) and backported with 8b80cfd5ea632d24c358ad05b52a80afbf4fee0b (PR #9231): when there are multiple output parameters processAlgorithm incorrectly generates multiple saga_cmd commands, instead of a single command containing the output parameters.

Fixes #33658

python/plugins/processing/algs/saga/SagaAlgorithm.py
@@ -326,7 +326,7 @@ class SagaAlgorithm(SagaAlgorithmBase):
 
             output_files[out.name()] = filePath
             command += ' -{} "{}"'.format(out.name(), filePath)
-            commands.append(command)
+        commands.append(command)
 
         # special treatment for RGB algorithm
         # TODO: improve this and put this code somewhere else