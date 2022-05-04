carsmie/QGIS

commit 6e6b2dc407f68e577c70824370576b0b5c83433b

use source model indexes in virtual  protected slots

src/gui/providers/ogr/qgsogrdbsourceselect.cpp
@@ -93,7 +93,8 @@ void QgsOgrDbSourceSelect::cbxAllowGeometrylessTables_stateChanged( int )
 
 void QgsOgrDbSourceSelect::treeviewClicked( const QModelIndex &index )
 {
-  mBuildQueryButton->setEnabled( index.parent().isValid() && mTablesTreeView->currentIndex().data( Qt::UserRole + 2 ) != QLatin1String( "Raster" ) );
+  const QString layerType = mTableModel->itemFromIndex( index )->data( Qt::UserRole + 2 ).toString();
+  mBuildQueryButton->setEnabled( index.parent().isValid() && layerType != QLatin1String( "Raster" ) );
 }
 
 void QgsOgrDbSourceSelect::treeviewDoubleClicked( const QModelIndex &index )
@@ -305,11 +306,10 @@ void QgsOgrDbSourceSelect::btnConnect_clicked()
 
 void QgsOgrDbSourceSelect::setSql( const QModelIndex &index )
 {
-  QModelIndex idx = proxyModel()->mapToSource( index );
-  QString tableName = mTableModel->itemFromIndex( idx.sibling( idx.row(), 0 ) )->text();
+  QString tableName = mTableModel->itemFromIndex( index.sibling( index.row(), 0 ) )->text();
 
   QgsVectorLayer::LayerOptions options { QgsProject::instance()->transformContext() };
-  std::unique_ptr<QgsVectorLayer> vlayer = std::make_unique<QgsVectorLayer>( layerURI( idx ), tableName, QStringLiteral( "ogr" ), options );
+  std::unique_ptr<QgsVectorLayer> vlayer = std::make_unique<QgsVectorLayer>( layerURI( index ), tableName, QStringLiteral( "ogr" ), options );
 
   if ( !vlayer->isValid() )
   {

src/providers/mssql/qgsmssqlsourceselect.cpp
@@ -532,11 +532,10 @@ void QgsMssqlSourceSelect::setSql( const QModelIndex &index )
     return;
   }
 
-  const QModelIndex idx = proxyModel()->mapToSource( index );
-  const QString tableName = mTableModel->itemFromIndex( idx.sibling( idx.row(), QgsMssqlTableModel::DbtmTable ) )->text();
+  const QString tableName = mTableModel->itemFromIndex( index.sibling( index.row(), QgsMssqlTableModel::DbtmTable ) )->text();
   const bool disableInvalidGeometryHandling = QgsMssqlConnection::isInvalidGeometryHandlingDisabled( cmbConnections->currentText() );
   const QgsVectorLayer::LayerOptions options { QgsProject::instance()->transformContext() };
-  std::unique_ptr< QgsVectorLayer > vlayer = std::make_unique< QgsVectorLayer>( mTableModel->layerURI( idx, mConnInfo, mUseEstimatedMetadata, disableInvalidGeometryHandling ), tableName, QStringLiteral( "mssql" ), options );
+  std::unique_ptr< QgsVectorLayer > vlayer = std::make_unique< QgsVectorLayer>( mTableModel->layerURI( index, mConnInfo, mUseEstimatedMetadata, disableInvalidGeometryHandling ), tableName, QStringLiteral( "mssql" ), options );
 
   if ( !vlayer->isValid() )
   {

src/providers/spatialite/qgsspatialitesourceselect.cpp
@@ -431,8 +431,7 @@ QString QgsSpatiaLiteSourceSelect::connectionInfo()
 
 void QgsSpatiaLiteSourceSelect::setSql( const QModelIndex &index )
 {
-  const QModelIndex idx = proxyModel()->mapToSource( index );
-  const auto item { mTableModel->itemFromIndex( idx.sibling( idx.row(), 0 ) ) };
+  const auto item { mTableModel->itemFromIndex( index.sibling( index.row(), 0 ) ) };
   if ( !item )
   {
     return;
@@ -441,7 +440,7 @@ void QgsSpatiaLiteSourceSelect::setSql( const QModelIndex &index )
   const QString tableName = item->text();
 
   const QgsVectorLayer::LayerOptions options { QgsProject::instance()->transformContext() };
-  QgsVectorLayer *vlayer = new QgsVectorLayer( layerURI( idx ), tableName, QStringLiteral( "spatialite" ), options );
+  QgsVectorLayer *vlayer = new QgsVectorLayer( layerURI( index ), tableName, QStringLiteral( "spatialite" ), options );
 
   if ( !vlayer->isValid() )
   {

src/gui/providers/qgsabstractdbsourceselect.cpp
@@ -45,10 +45,14 @@ QgsAbstractDbSourceSelect::QgsAbstractDbSourceSelect( QWidget *parent, Qt::Windo
   mBuildQueryButton->setDisabled( true );
   buttonBox->addButton( mBuildQueryButton, QDialogButtonBox::ActionRole );
 
-  connect( mTablesTreeView, &QTreeView::clicked, this, &QgsAbstractDbSourceSelect::treeviewClicked );
-  connect( mTablesTreeView, &QTreeView::doubleClicked, this, &QgsAbstractDbSourceSelect::treeviewDoubleClicked );
+  connect( mTablesTreeView, &QTreeView::clicked, this, [ = ](const QModelIndex &index){
+    treeviewClicked( mProxyModel->mapToSource( index ) );
+  });
+  connect( mTablesTreeView, &QTreeView::doubleClicked, this, [ = ](const QModelIndex &index){
+    treeviewDoubleClicked( mProxyModel->mapToSource( index ) );
+  });
 
-  connect( mBuildQueryButton, &QAbstractButton::clicked, this, [ = ]() {setSql( mTablesTreeView->currentIndex() );} );
+  connect( mBuildQueryButton, &QAbstractButton::clicked, this, [ = ]() {setSql( mProxyModel->mapToSource(mTablesTreeView->currentIndex() ));} );
 }
 
 void QgsAbstractDbSourceSelect::init( QgsAbstractDbTableModel *model, QItemDelegate *delegate )

src/gui/providers/qgsabstractdbsourceselect.h
@@ -47,10 +47,15 @@ class GUI_EXPORT QgsAbstractDbSourceSelect : public QgsAbstractDataSourceWidget,
     QPushButton *mBuildQueryButton = nullptr;
 
   protected slots:
-    //! This is called to define the SQL query and must be re-implemented. The implementation should call QgsAbstractDbTableModel::setSql
+    //! This is called to define the SQL query and must be re-implemented.
+    //! The implementation should call QgsAbstractDbTableModel::setSql
+    //! The index is in the source model.
     virtual void setSql( const QModelIndex &index ) = 0;
 
+    //! Called on click for the source \a index
     virtual void treeviewClicked( const QModelIndex &index );
+
+    //! Called on double click for the source \a index
     virtual void treeviewDoubleClicked( const QModelIndex &index );
 
   private:

src/providers/db2/qgsdb2sourceselect.cpp
@@ -460,11 +460,10 @@ void QgsDb2SourceSelect::setSql( const QModelIndex &index )
     return;
   }
 
-  const QModelIndex idx = proxyModel()->mapToSource( index );
-  const QString tableName = mTableModel->itemFromIndex( idx.sibling( idx.row(), QgsDb2TableModel::DbtmTable ) )->text();
+  const QString tableName = mTableModel->itemFromIndex( index.sibling( index.row(), QgsDb2TableModel::DbtmTable ) )->text();
 
   const QgsVectorLayer::LayerOptions options { QgsProject::instance()->transformContext() };
-  std::unique_ptr< QgsVectorLayer > vlayer = std::make_unique< QgsVectorLayer >( mTableModel->layerURI( idx, mConnInfo, mUseEstimatedMetadata ), tableName, QStringLiteral( "DB2" ), options );
+  std::unique_ptr< QgsVectorLayer > vlayer = std::make_unique< QgsVectorLayer >( mTableModel->layerURI( index, mConnInfo, mUseEstimatedMetadata ), tableName, QStringLiteral( "DB2" ), options );
 
   if ( !vlayer->isValid() )
   {

src/providers/postgres/qgspgsourceselect.cpp
@@ -530,10 +530,9 @@ void QgsPgSourceSelect::setSql( const QModelIndex &index )
     return;
   }
 
-  QModelIndex idx = proxyModel()->mapToSource( index );
-  QString tableName = mTableModel->itemFromIndex( idx.sibling( idx.row(), QgsPgTableModel::DbtmTable ) )->text();
+  QString tableName = mTableModel->itemFromIndex( index.sibling( index.row(), QgsPgTableModel::DbtmTable ) )->text();
 
-  QString uri = mTableModel->layerURI( idx, connectionInfo( false ), mUseEstimatedMetadata );
+  QString uri = mTableModel->layerURI( index, connectionInfo( false ), mUseEstimatedMetadata );
   if ( uri.isNull() )
   {
     QgsDebugMsg( QStringLiteral( "no uri" ) );

commit c51b3b4d5560005af25dc6063a9dac0603c9235a

Fix setSql for all providers
Broken since commit 6e6b2dc

src/gui/providers/ogr/qgsogrdbsourceselect.cpp
@@ -321,7 +321,7 @@ void QgsOgrDbSourceSelect::setSql( const QModelIndex &index )
 
   if ( gb->exec() )
   {
-    mTableModel->setSql( proxyModel()->mapToSource( index ), gb->sql() );
+    mTableModel->setSql( index, gb->sql() );
   }
 }
 

src/providers/mssql/qgsmssqlsourceselect.cpp
@@ -546,7 +546,7 @@ void QgsMssqlSourceSelect::setSql( const QModelIndex &index )
   QgsQueryBuilder gb( vlayer.get(), this );
   if ( gb.exec() )
   {
-    mTableModel->setSql( proxyModel()->mapToSource( index ), gb.sql() );
+    mTableModel->setSql( index, gb.sql() );
   }
 }
 

src/providers/oracle/qgsoraclesourceselect.cpp
@@ -472,10 +472,9 @@ void QgsOracleSourceSelect::setSql( const QModelIndex &index )
     return;
   }
 
-  QModelIndex idx = proxyModel()->mapToSource( index );
-  QString tableName = mTableModel->itemFromIndex( idx.sibling( idx.row(), QgsOracleTableModel::DbtmTable ) )->text();
+  QString tableName = mTableModel->itemFromIndex( index.sibling( index.row(), QgsOracleTableModel::DbtmTable ) )->text();
 
-  QString uri = mTableModel->layerURI( idx, mConnInfo );
+  QString uri = mTableModel->layerURI( index, mConnInfo );
   if ( uri.isNull() )
   {
     QgsDebugMsg( QStringLiteral( "no uri" ) );
@@ -493,7 +492,7 @@ void QgsOracleSourceSelect::setSql( const QModelIndex &index )
   QgsQueryBuilder *gb = new QgsQueryBuilder( vlayer, this );
   if ( gb->exec() )
   {
-    mTableModel->setSql( proxyModel()->mapToSource( index ), gb->sql() );
+    mTableModel->setSql( index, gb->sql() );
   }
 
   delete gb;

src/providers/spatialite/qgsspatialitesourceselect.cpp
@@ -452,7 +452,7 @@ void QgsSpatiaLiteSourceSelect::setSql( const QModelIndex &index )
   QgsQueryBuilder *gb = new QgsQueryBuilder( vlayer, this );
   if ( gb->exec() )
   {
-    mTableModel->setSql( proxyModel()->mapToSource( index ), gb->sql() );
+    mTableModel->setSql( index, gb->sql() );
   }
 
   delete gb;