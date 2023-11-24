carsmie/QGIS

commit d7803986938cfb533ae77b481fc737a868016b5d

qgscameracontroller: Fix combination of rotation and zoom with the mouse
While pressing the left button of the mouse, some sequences do not
work properly.
For example, : rotate around a clicked point, zoom-in and then rotate
again. The last rotation does not work because a new rotation
center (mRotationCenter) has not been computed. This is because the
`mRotationCenterCalculated` flag has not been updated.

This issue is fixed by calling
`QgsCameraController::setMouseParameters` accordingly in
`QgsCameraController::onPositionChangedTerrainNavigation`.

src/3d/qgscameracontroller.cpp
@@ -325,6 +325,8 @@ void QgsCameraController::onPositionChangedTerrainNavigation( Qt3DInput::QMouseE
   {
     // rotate/tilt using mouse (camera moves as it rotates around the clicked point)
 
+    setMouseParameters( MouseOperation::Rotation );
+
     float scale = static_cast<float>( std::max( mScene->engine()->size().width(), mScene->engine()->size().height() ) );
     float pitchDiff = 180.0f * static_cast<float>( mouse->y() - mMiddleButtonClickPos.y() ) / scale;
     float yawDiff = -180.0f * static_cast<float>( mouse->x() - mMiddleButtonClickPos.x() ) / scale;
@@ -386,6 +388,7 @@ void QgsCameraController::onPositionChangedTerrainNavigation( Qt3DInput::QMouseE
   else if ( hasLeftButton && !hasShift && !hasCtrl )
   {
     // translation works as if one grabbed a point on the 3D viewer and dragged it
+    setMouseParameters( MouseOperation::Translation );
 
     if ( !mDepthBufferIsReady )
       return;
@@ -444,6 +447,7 @@ void QgsCameraController::onPositionChangedTerrainNavigation( Qt3DInput::QMouseE
   }
   else if ( hasRightButton && !hasShift && !hasCtrl )
   {
+    setMouseParameters( MouseOperation::Zoom );
     if ( !mDepthBufferIsReady )
       return;
 

commit 12ed4a2a1a487f6b2c3445fc92c5168e7a271b09

qgscameracontroller: Distinguish zoom with a click from zoom on wheel
The `depthBufferCaptured` logic needs to distinguish a zoom made while
pressing the right button from a zoom with a wheel otherwise the
`onPositionChangedTerrainNavigation` function is never called.

This fixes a regression introduced by commit
d7803986938cfb533ae77b481fc737a868016b5d

src/3d/qgscameracontroller.cpp
@@ -595,9 +595,9 @@ void QgsCameraController::onWheel( Qt3DInput::QWheelEvent *wheel )
       // see: https://doc.qt.io/qt-5/qwheelevent.html#angleDelta
       mCumulatedWheelY += scaling * wheel->angleDelta().y();
 
-      if ( mCurrentOperation != MouseOperation::Zoom )
+      if ( mCurrentOperation != MouseOperation::ZoomWheel )
       {
-        setMouseParameters( MouseOperation::Zoom );
+        setMouseParameters( MouseOperation::ZoomWheel );
       }
       else
       {
@@ -1045,7 +1045,7 @@ void QgsCameraController::depthBufferCaptured( const QImage &depthImage )
   mDepthBufferImage = depthImage;
   mDepthBufferIsReady = true;
 
-  if ( mCurrentOperation == MouseOperation::Zoom )
+  if ( mCurrentOperation == MouseOperation::ZoomWheel )
   {
     handleTerrainNavigationWheelZoom();
   }

src/3d/qgscameracontroller.h
@@ -230,7 +230,8 @@ class _3D_EXPORT QgsCameraController : public QObject
       None = 0,
       Translation,
       Rotation,
-      Zoom
+      Zoom,
+      ZoomWheel
     };
 
     void setMouseParameters( const MouseOperation &newOperation, const QPoint &clickPoint = QPoint() );