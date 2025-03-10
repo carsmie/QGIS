carsmie/QGIS

commit 8eca77dfd6e53f7c0420c0f67cd5868fb3a1e900

Fix memory leaks in classification methods, use unique_ptrs

src/gui/symbology/qgsgraduatedsymbolrendererwidget.cpp
@@ -872,7 +872,7 @@ void QgsGraduatedSymbolRendererWidget::updateMethodParameters()
   clearParameterWidgets();
 
   const QString methodId = cboGraduatedMode->currentData().toString();
-  QgsClassificationMethod *method = QgsApplication::classificationMethodRegistry()->method( methodId );
+  std::unique_ptr< QgsClassificationMethod > method = QgsApplication::classificationMethodRegistry()->method( methodId );
   Q_ASSERT( method );
 
   // need more context?
@@ -1044,7 +1044,7 @@ void QgsGraduatedSymbolRendererWidget::classifyGraduatedImpl()
   int nclasses = spinGraduatedClasses->value();
 
   const QString methodId = cboGraduatedMode->currentData().toString();
-  QgsClassificationMethod *method = QgsApplication::classificationMethodRegistry()->method( methodId );
+  std::unique_ptr< QgsClassificationMethod > method = QgsApplication::classificationMethodRegistry()->method( methodId );
   Q_ASSERT( method );
 
   int attrNum = mLayer->fields().lookupField( attrName );
@@ -1081,14 +1081,14 @@ void QgsGraduatedSymbolRendererWidget::classifyGraduatedImpl()
   method->setParameterValues( parameterValues );
 
   // set method to renderer
-  mRenderer->setClassificationMethod( method );
+  mRenderer->setClassificationMethod( method.release() );
 
   // create and set new renderer
   mRenderer->setClassAttribute( attrName );
 
   // If complexity >= oN^2, warn for big dataset (more than 50k records)
   // and give the user the chance to cancel
-  if ( method->codeComplexity() > 1 && mLayer->featureCount() > 50000 )
+  if ( mRenderer->classificationMethod()->codeComplexity() > 1 && mLayer->featureCount() > 50000 )
   {
     if ( QMessageBox::Cancel == QMessageBox::question( this, tr( "Apply Classification" ), tr( "Natural break classification (Jenks) is O(n2) complexity, your classification may take a long time.\nPress cancel to abort breaks calculation or OK to continue." ), QMessageBox::Cancel, QMessageBox::Ok ) )
     {

python/PyQt6/core/auto_generated/classification/qgsclassificationcustom.sip.in
@@ -24,7 +24,7 @@ which does not compute any break.
   public:
     QgsClassificationCustom();
 
-    virtual QgsClassificationMethod *clone() const;
+    virtual std::unique_ptr< QgsClassificationMethod > clone() const;
 
     virtual QString name() const;
 

python/PyQt6/core/auto_generated/classification/qgsclassificationequalinterval.sip.in
@@ -27,7 +27,7 @@ for equal intervals
 
     virtual QString id() const;
 
-    virtual QgsClassificationMethod *clone() const;
+    virtual std::unique_ptr< QgsClassificationMethod > clone() const;
 
     virtual QIcon icon() const;
 

python/PyQt6/core/auto_generated/classification/qgsclassificationfixedinterval.sip.in
@@ -23,7 +23,7 @@ Implementation of a fixed interval classification.
   public:
 
     QgsClassificationFixedInterval();
-    virtual QgsClassificationMethod *clone() const;
+    virtual std::unique_ptr< QgsClassificationMethod > clone() const;
 
     virtual QString name() const;
 

python/PyQt6/core/auto_generated/classification/qgsclassificationjenks.sip.in
@@ -27,7 +27,7 @@ for natural breaks based on Jenks method
 
     virtual QString id() const;
 
-    virtual QgsClassificationMethod *clone() const;
+    virtual std::unique_ptr< QgsClassificationMethod > clone() const;
 
     virtual QIcon icon() const;
 

python/PyQt6/core/auto_generated/classification/qgsclassificationlogarithmic.sip.in
@@ -30,7 +30,7 @@ Implementation of a logarithmic scale method
     };
 
     QgsClassificationLogarithmic();
-    virtual QgsClassificationMethod *clone() const;
+    virtual std::unique_ptr< QgsClassificationMethod > clone() const;
 
     virtual QString name() const;
 

python/PyQt6/core/auto_generated/classification/qgsclassificationmethod.sip.in
@@ -123,7 +123,7 @@ Creates a classification method.
 
     virtual ~QgsClassificationMethod();
 
-    virtual QgsClassificationMethod *clone() const = 0 /Factory/;
+    virtual std::unique_ptr< QgsClassificationMethod > clone() const = 0;
 %Docstring
 Returns a clone of the method.
 Implementation can take advantage of copyBase method which copies the parameters of the base class
@@ -296,7 +296,7 @@ Saves the method to a DOM element and return it
 :param context: the read/write context
 %End
 
-    static QgsClassificationMethod *create( const QDomElement &element, const QgsReadWriteContext &context ) /Factory/;
+    static std::unique_ptr< QgsClassificationMethod > create( const QDomElement &element, const QgsReadWriteContext &context );
 %Docstring
 Reads the DOM element and return a new classification method from it
 

python/PyQt6/core/auto_generated/classification/qgsclassificationmethodregistry.sip.in
@@ -36,7 +36,7 @@ Adds a method to the registry
 Returns ``False`` if a method with same id already exists.
 %End
 
-    QgsClassificationMethod *method( const QString &id ) /Factory/;
+    std::unique_ptr< QgsClassificationMethod > method( const QString &id );
 %Docstring
 Returns a new instance of the method for the given id
 %End

commit 72696fca3fa4046d59e66a53b4a8b774b53c4ebd

Fix crash in graduated symbol renderer widget
Caused by 8eca77d -- the previous code was relying on the leak
to reference processing parameter owned by the leaked object

Fixes #60865

src/gui/symbology/qgsgraduatedsymbolrendererwidget.cpp
@@ -877,18 +877,18 @@ void QgsGraduatedSymbolRendererWidget::updateMethodParameters()
   clearParameterWidgets();
 
   const QString methodId = cboGraduatedMode->currentData().toString();
-  std::unique_ptr< QgsClassificationMethod > method = QgsApplication::classificationMethodRegistry()->method( methodId );
-  Q_ASSERT( method );
+  mClassificationMethod = QgsApplication::classificationMethodRegistry()->method( methodId );
+  Q_ASSERT( mClassificationMethod.get() );
 
   // need more context?
   QgsProcessingContext context;
 
-  for ( const QgsProcessingParameterDefinition *def : method->parameterDefinitions() )
+  for ( const QgsProcessingParameterDefinition *def : mClassificationMethod->parameterDefinitions() )
   {
     QgsAbstractProcessingParameterWidgetWrapper *ppww = QgsGui::processingGuiRegistry()->createParameterWidgetWrapper( def, QgsProcessingGui::Standard );
     mParametersLayout->addRow( ppww->createWrappedLabel(), ppww->createWrappedWidget( context ) );
 
-    QVariant value = method->parameterValues().value( def->name(), def->defaultValueForGui() );
+    QVariant value = mClassificationMethod->parameterValues().value( def->name(), def->defaultValueForGui() );
     ppww->setParameterValue( value, context );
 
     connect( ppww, &QgsAbstractProcessingParameterWidgetWrapper::widgetValueHasChanged, this, &QgsGraduatedSymbolRendererWidget::classifyGraduated );
@@ -896,7 +896,7 @@ void QgsGraduatedSymbolRendererWidget::updateMethodParameters()
     mParameterWidgetWrappers.push_back( std::unique_ptr<QgsAbstractProcessingParameterWidgetWrapper>( ppww ) );
   }
 
-  spinGraduatedClasses->setEnabled( !( method->flags() & QgsClassificationMethod::MethodProperty::IgnoresClassCount ) );
+  spinGraduatedClasses->setEnabled( !( mClassificationMethod->flags() & QgsClassificationMethod::MethodProperty::IgnoresClassCount ) );
 }
 
 void QgsGraduatedSymbolRendererWidget::toggleMethodWidgets( MethodMode mode )
@@ -1041,16 +1041,12 @@ void QgsGraduatedSymbolRendererWidget::classifyGraduated()
 
 void QgsGraduatedSymbolRendererWidget::classifyGraduatedImpl()
 {
-  if ( mBlockUpdates )
+  if ( mBlockUpdates || !mClassificationMethod )
     return;
 
   QgsTemporaryCursorOverride override( Qt::WaitCursor );
   QString attrName = mExpressionWidget->currentField();
   int nclasses = spinGraduatedClasses->value();
-
-  const QString methodId = cboGraduatedMode->currentData().toString();
-  std::unique_ptr< QgsClassificationMethod > method = QgsApplication::classificationMethodRegistry()->method( methodId );
-  Q_ASSERT( method );
 
   int attrNum = mLayer->fields().lookupField( attrName );
 
@@ -1064,29 +1060,29 @@ void QgsGraduatedSymbolRendererWidget::classifyGraduatedImpl()
   mSymmetryPointValidator->setTop( maximum );
   mSymmetryPointValidator->setMaxDecimals( spinPrecision->value() );
 
-  if ( method->id() == QgsClassificationEqualInterval::METHOD_ID || method->id() == QgsClassificationStandardDeviation::METHOD_ID )
+  if ( mClassificationMethod->id() == QgsClassificationEqualInterval::METHOD_ID || mClassificationMethod->id() == QgsClassificationStandardDeviation::METHOD_ID )
   {
     // knowing that spinSymmetryPointForOtherMethods->value() is automatically put at minimum when out of min-max
     // using "(maximum-minimum)/100)" to avoid direct comparison of doubles
     double currentValue = QgsDoubleValidator::toDouble( cboSymmetryPoint->currentText() );
     if ( currentValue < ( minimum + ( maximum - minimum ) / 100. ) || currentValue > ( maximum - ( maximum - minimum ) / 100. ) )
-      cboSymmetryPoint->setItemText( cboSymmetryPoint->currentIndex(), QLocale().toString( minimum + ( maximum - minimum ) / 2., 'f', method->labelPrecision() + 2 ) );
+      cboSymmetryPoint->setItemText( cboSymmetryPoint->currentIndex(), QLocale().toString( minimum + ( maximum - minimum ) / 2., 'f', mClassificationMethod->labelPrecision() + 2 ) );
   }
 
   if ( mGroupBoxSymmetric->isChecked() )
   {
     double symmetryPoint = QgsDoubleValidator::toDouble( cboSymmetryPoint->currentText() );
     bool astride = cbxAstride->isChecked();
-    method->setSymmetricMode( true, symmetryPoint, astride );
+    mClassificationMethod->setSymmetricMode( true, symmetryPoint, astride );
   }
 
   QVariantMap parameterValues;
   for ( const auto &ppww : std::as_const( mParameterWidgetWrappers ) )
     parameterValues.insert( ppww->parameterDefinition()->name(), ppww->parameterValue() );
-  method->setParameterValues( parameterValues );
+  mClassificationMethod->setParameterValues( parameterValues );
 
   // set method to renderer
-  mRenderer->setClassificationMethod( method.release() );
+  mRenderer->setClassificationMethod( mClassificationMethod->clone().release() );
 
   // create and set new renderer
   mRenderer->setClassAttribute( attrName );

src/gui/symbology/qgsgraduatedsymbolrendererwidget.h
@@ -203,6 +203,8 @@ class GUI_EXPORT QgsGraduatedSymbolRendererWidget : public QgsRendererWidget, pr
 
     QgsDoubleValidator *mSymmetryPointValidator = nullptr;
     QAction *mActionLevels = nullptr;
+
+    std::unique_ptr< QgsClassificationMethod > mClassificationMethod;
     std::vector<std::unique_ptr<QgsAbstractProcessingParameterWidgetWrapper>> mParameterWidgetWrappers;
 
     int mBlockUpdates = 0;